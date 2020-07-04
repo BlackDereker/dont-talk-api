85b5238271e52ef72bbb5bc054182d5c33c08f72
Bruno Godoi Machado <blackdereker@gmail.com>
2020-07-04 04:20:35 -0300

    Fixed imports and requirements

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -2,7 +2,6 @@ from flask import Blueprint
 from flask_restful import Api
 
 from resources.ping import Ping
-from resources.report import Report
 
 api_bp = Blueprint("api", __name__)
 api = Api(api_bp)
